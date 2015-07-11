d70841791f96f9e589d648786428fea8f90be0ee
Oanes <privat@kevin-krings.de>
2015-07-11 21:47:56 +0200

    Anpassen der Testdaten

--- TypeScriptHTMLApp3/Module/Process/Service/ProcessStepChart.js
+++ TypeScriptHTMLApp3/Module/Process/Service/ProcessStepChart.js
@@ -9,7 +9,7 @@ var y = d3.scale.linear()
 	.rangeRound([height, 0]);
 
 var color = d3.scale.ordinal()
-	.range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
+	.range(["#515151", "#319027", "#F5180C"]);
 
 var xAxis = d3.svg.axis()
 	.scale(x)
@@ -55,7 +55,7 @@ d3.csv("chart2_demo.csv", function (error, data) {
 		.attr("y", 6)
 		.attr("dy", ".71em")
 		.style("text-anchor", "end")
-		.text("Population");
+		.text("Kosten");
 
 	var state = svg.selectAll(".state")
 		.data(data)
